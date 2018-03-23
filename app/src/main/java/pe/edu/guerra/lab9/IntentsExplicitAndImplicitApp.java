b7c60b13144a0d1b7f6cfcfa37d76caa49f5495a
Linder Hassinger <linderhassinger00@gmail.com>
2018-03-23 14:31:10 -0500

    2:40
    Signed-off-by: Linder Hassinger <linderhassinger00@gmail.com>

--- app/src/main/java/pe/edu/guerra/lab9/IntentsExplicitAndImplicitApp.java
+++ app/src/main/java/pe/edu/guerra/lab9/IntentsExplicitAndImplicitApp.java
@@ -1,13 +1,64 @@
 package pe.edu.guerra.lab9;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class IntentsExplicitAndImplicitApp extends AppCompatActivity {
+
+    private EditText codeInput;
+    private EditText fullnameInput;
+    private EditText amountInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intents_explicit_and_implicit_app);
+
+
+
+        codeInput = (EditText)findViewById(R.id.codeInput);
+        fullnameInput = (EditText)findViewById(R.id.fullnameInput);
+        amountInput = (EditText)findViewById(R.id.amountInput);
+
     }
+
+    public void sendExplicit(View view){
+
+        String code = codeInput.getText().toString();
+        String fullname = fullnameInput.getText().toString();
+        String amount = amountInput.getText().toString();
+
+        if("".equals(code) || "".equals(fullname) || "".equals(amount)){
+            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("code", Integer.parseInt(code));
+        intent.putExtra("fullname", fullname);
+        intent.putExtra("amount", Double.parseDouble(amount));
+        startActivity(intent);
+    }
+
+    public void sendImplicit(View view){
+
+        String code = codeInput.getText().toString();
+        String fullname = fullnameInput.getText().toString();
+        String amount = amountInput.getText().toString();
+
+        if("".equals(code) || "".equals(fullname) || "".equals(amount)){
+            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "code: "+code+"\nfullname: "+fullname+"\namount: "+amount);
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/pe/edu/guerra/lab9/MainActivity.java
+++ app/src/main/java/pe/edu/guerra/lab9/MainActivity.java
@@ -1,7 +1,11 @@
 package pe.edu.guerra.lab9;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callActivityA(View view){
+        Log.d(TAG, "callActivityA");
+        startActivityForResult(new Intent(this, PantallaAActivity.class), ACTIVITY_A_REQUEST);
+    }
+    public void callActivityB(View view){
+        Log.d(TAG, "callActivityB");
+        startActivityForResult(new Intent(this, PantallaBActivity.class), ACTIVITY_B_REQUEST);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG, "requestCode:"+requestCode);
+        Log.d(TAG, "resultCode:"+resultCode);
+        Log.d(TAG, "data:"+data);
+  
+        switch (requestCode){
+            case ACTIVITY_A_REQUEST:
+                Log.d(TAG, "Regresamos del Activity A");
+                break;
+            case ACTIVITY_B_REQUEST:
+                Log.d(TAG, "Regresamos del Activity B");
+
+                if(resultCode == RESULT_OK){
+                    String valor = data.getExtras().getString("valor");
+                    Log.d(TAG, "valor: " + valor);
+                }
+
+                break;
+        }
+
     }
 }
